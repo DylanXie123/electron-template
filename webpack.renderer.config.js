effaebbbb09d473b0067d83db03100cc533f2d74
DylanXie123 <dylanxie123@outlook.com>
2022-10-05 11:53:26 +0800

    feat(react): finish react template
    react typescript webpack bootstrap

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,13 +1,32 @@
 const rules = require('./webpack.rules');
+const Dotenv = require('dotenv-webpack');
 
-rules.push({
-  test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-});
+rules.push(
+  {
+    test: /\.(s[ac]ss|css)$/i,
+    use: [
+      { loader: "css-loader", options: { sourceMap: true }, },
+      "sass-loader",
+    ],
+  },
+  {
+    test: /\.(png|svg|jpg|jpeg|gif)$/i,
+    type: 'asset/resource',
+  }
+);
 
 module.exports = {
   // Put your normal webpack config below here
   module: {
     rules,
   },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx', '.json'],
+    fallback: {
+      path: require.resolve('path-browserify'),
+    }
+  },
+  plugins: [
+    new Dotenv(),
+  ],
 };
